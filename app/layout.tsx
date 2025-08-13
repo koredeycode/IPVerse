1a6558f1a7c90d5c765612a97076e161a1bea54c
Yusuf Akorede <koredey4u@gmail.com>
2025-08-13 11:36:06 +0100

    add react Suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { CampModal } from "@campnetwork/origin/react";
 import { Toaster } from "sonner";
 import Providers from "./providers";
+import { Suspense } from "react";
 // import { Head } from "next/document";
 
 const geistSans = Geist({
@@ -32,11 +33,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>
-          <CampModal injectButton={false} />
-          {children}
-        </Providers>
-        <Toaster theme="dark" />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Providers>
+            <CampModal injectButton={false} />
+            {children}
+          </Providers>
+          <Toaster theme="dark" />
+        </Suspense>
       </body>
     </html>
   );
